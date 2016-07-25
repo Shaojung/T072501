63afad0405be798d506e9fe60975cb91e4814f7d
shaojung <shaojung@gmail.com>
2016-07-25 14:23:32 +0800

    輸入型對話框

--- app/src/main/java/tw/com/pcschool/t072501/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t072501/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +40,33 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        builder.create().show();
+    }
+
+    public void click2(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("輸入型對話框");
+        builder.setMessage("這是訊息");
+
+        final EditText ed = new EditText(MainActivity.this);
+        builder.setView(ed);
+
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String str = ed.getText().toString();
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
 
         builder.create().show();
     }
